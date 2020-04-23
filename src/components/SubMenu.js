dd670f914269cfeb941dadc785eedf9838187f00
4leaf.ysh <4leaf.ysh@gmail.com>
2020-04-23 17:24:31 +0900

    add line styles.css

---
+++ src/components/SubMenu.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class SubMenu extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>asdfasdf</div>
+      </div>
+    );
+  }
+}
+
+export default SubMenu;
--- src/components/screens/SC04/LS0401.js
+++ src/components/screens/SC04/LS0401.js
@@ -1,10 +1,11 @@
 import React from "react";
+import SubMenu from "../../SubMenu";
 
 class LS0401 extends React.Component {
   render() {
     return (
       <div>
-        <div>LS0401</div>
+        <SubMenu />
       </div>
     );
   }
