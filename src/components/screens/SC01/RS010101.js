5735dd575a8a3226eb438857cf77039b78355f5d
song <songsm5504@gmail.com>
2020-04-24 18:30:09 +0900

    0101

--- src/components/screens/SC01/RS010101.js
+++ src/components/screens/SC01/RS010101.js
@@ -4,12 +4,12 @@ class RS0101 extends React.Component {
   render() {
     return (
       <div className="RS010101__screen">
-        <div>
+        <div className="RS010101__content">
           <div className="RS010101__mainTitle">#비전</div>
           <div>한번 더 눈이가는, 계속 보고싶은</div>
           <div>
-            고객과 풍부한 소통, 끈끈한 연결을 통하여 생산성 증가와 더 나은
-            결과물을 창출하는 것에 초점을 두고 있습니다.
+            고객과 풍부한 소통, 끈끈한 연결을 통하여 <br />
+            생산성 증가와 더 나은 결과물을 창출하는 것에 초점을 두고 있습니다.
           </div>
         </div>
       </div>
