2c26e75569db749784abbafbdad421d5d26cdab7
Kim Hanju <khj64076407@gmail.com>
2020-04-24 18:16:09 +0900

    menu

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -2,11 +2,18 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 class MenuBar extends React.Component {
+  state = {
+    isMenuOpen: false,
+    openBarClassName: "openMenuBar",
+    closeBarClassName: "closeMenuBar",
+  };
+
   constructor(props) {
     super(props);
   }
 
   render() {
+    const { isMenuOpen, openBarClassName, closeBarClassName } = this.state;
     return (
       <div className="ma1__menu">
         <div className="ma1__menu__logo">
@@ -14,54 +21,73 @@ class MenuBar extends React.Component {
             <NavLink to="/">LOGO</NavLink>
           </div>
         </div>
-        <input type="checkbox" id="menu_button" />
-        <label
-          htmlFor="menu_button"
-          id="menu_button"
-          onClick={() => this._visibleMenuBar()}
-        >
+        <div className={openBarClassName} onClick={() => this._visibleMenu()}>
           <span className="line"></span>
           <span className="line2"></span>
           <span className="line"></span>
-        </label>
-        <div
-          className="ma1__menu__link"
-          id="menu"
-          onClick={() => this._disableMenuBar()}
-        >
-          <label htmlFor="menu_button" id="menu_close">
-            <span className="line"></span>
-            <span className="line2"></span>
-            <span className="line"></span>
-          </label>
-          <div className="menu__link__list">
-            <div onClick={() => this.props.setMenuFunc()}>
-              <NavLink to="/intro">회사소개</NavLink>
+        </div>
+
+        {isMenuOpen ? (
+          <div className="ma1__menu__link" id="menu">
+            <div
+              className={closeBarClassName}
+              id="openMenuBar-js"
+              onClick={() => [this._visibleMenu(), this._clickClose()]}
+            >
+              <span className="line"></span>
+              <span className="line2"></span>
+              <span className="line"></span>
             </div>
-            <div onClick={() => this.props.setMenuFunc()}>
-              <NavLink to="/software">소프트웨어</NavLink>
-            </div>
-            <div onClick={() => this.props.setMenuFunc()}>
-              <NavLink to="/community">커뮤니티</NavLink>
-            </div>
-            <div onClick={() => this.props.setMenuFunc()}>
-              <NavLink to="/support">고객지원</NavLink>
+            <div
+              className="menu__link__list"
+              onClick={() => this._visibleMenu()}
+            >
+              <div onClick={() => this.props.setMenuFunc()}>
+                <NavLink to="/intro">회사소개</NavLink>
+              </div>
+              <div onClick={() => this.props.setMenuFunc()}>
+                <NavLink to="/software">소프트웨어</NavLink>
+              </div>
+              <div onClick={() => this.props.setMenuFunc()}>
+                <NavLink to="/community">커뮤니티</NavLink>
+              </div>
+              <div onClick={() => this.props.setMenuFunc()}>
+                <NavLink to="/support">고객지원</NavLink>
+              </div>
             </div>
           </div>
-        </div>
+        ) : null}
       </div>
     );
   }
 
-  _visibleMenuBar = () => {
-    const menuDiv = document.getElementById("menu");
-    menuDiv.style.display = "none";
-    menuDiv.style.display = "block";
+  _clickClose = () => {
+    this.setState({
+      openBarClassName: "openMenuBar",
+    });
   };
 
-  _disableMenuBar = () => {
-    const menuDiv = document.getElementById("menu");
-    menuDiv.style.display = "none";
+  _visibleMenu = () => {
+    if (this.state.isMenuOpen) {
+      this.setState({
+        closeBarClassName: "closeMenuBar close",
+      });
+
+      setTimeout(() => {
+        this.setState({
+          isMenuOpen: !this.state.isMenuOpen,
+
+          openBarClassName: "openMenuBar",
+        });
+      }, 700);
+
+      return;
+    }
+
+    this.setState({
+      isMenuOpen: !this.state.isMenuOpen,
+      openBarClassName: "closeMenuBar",
+    });
   };
 }
 
