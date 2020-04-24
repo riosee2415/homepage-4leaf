ca971d62268b850db2838bf0d1e302f459709668
njm1128 <njm1128@naver.com>
2020-04-24 16:50:52 +0900

    Software screen css 연결

--- src/components/screens/SC02/LS0201.js
+++ src/components/screens/SC02/LS0201.js
@@ -1,10 +1,35 @@
 import React from "react";
+import SubMenu from "../../SubMenu";
+
+const menuList = [
+  {
+    title: "제품",
+    desc: "",
+    detailDesc:
+      ""
+  },
+  {
+    title: "솔루션",
+    desc: "",
+    detailDesc:
+      "
+  }
+];
 
 class LS0201 extends React.Component {
   render() {
     return (
-      <div>
-        <div>LS0201</div>
+      <div className="LS0201__screen">
+        {menuList.map((menu, idx) => {
+          return (
+            <SubMenu
+              key={idx}
+              title={menu.title}
+              desc={menu.desc}
+              detailDesc={menu.detailDesc}
+            />
+          );
+        })}
       </div>
     );
   }
