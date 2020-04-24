96624702e72cc83d8281afd5810b887a25176376
4leaf.ysh <4leaf.ysh@gmail.com>
2020-04-24 11:21:54 +0900

    Create SC0401 pages

--- src/components/SubMenu.js
+++ src/components/SubMenu.js
@@ -1,15 +1,23 @@
 import React from "react";
+import DoneAllIcon from "@material-ui/icons/DoneAll";
+import CallMissedOutgoingIcon from "@material-ui/icons/CallMissedOutgoing";
 
 class SubMenu extends React.Component {
   render() {
     return (
       <div className="subMenu">
         <div className="subMenu__up">
-          <div>{this.props.title}</div>
-          <div>{this.props.desc}</div>
+          <DoneAllIcon style={{ fontSize: 10 }} />
+          <div className="subMenu__up__title">{this.props.title}</div>
+          <div className="subMenu__up__desc">{this.props.desc}</div>
         </div>
         <div className="subMenu__down">
-          <span>{this.props.detailDesc}</span>
+          <span>
+            {this.props.detailDesc}
+            <span className="subMenu__down__icon">
+              <CallMissedOutgoingIcon style={{ fontSize: 40 }} />
+            </span>
+          </span>
         </div>
       </div>
     );
--- src/components/screens/SC04/LS0401.js
+++ src/components/screens/SC04/LS0401.js
@@ -1,11 +1,40 @@
 import React from "react";
 import SubMenu from "../../SubMenu";
 
+const menuList = [
+  {
+    title: "온라인 문의",
+    desc: "궁굼한 내용을 문의하세요.",
+    detailDesc:
+      "홈페이지 제작, 어플리케이션 개발 등 4LEAF에게 궁굼하신 사항이 있으신가요?",
+  },
+  {
+    title: "견적 문의",
+    desc: "상세 견적을 문의하세요.",
+    detailDesc:
+      "개발하고싶은 소프트웨어가 있으신가요? 4LEAF이 파트너가 되어드립니다.",
+  },
+  {
+    title: "자료실",
+    desc: "필요한 자료들이 있습니다.",
+    detailDesc:
+      "사용 메뉴얼, 템플릿 오픈소스 등 4LEAF의 다양한 자료들을 활용하세요.",
+  },
+];
+
 class LS0401 extends React.Component {
   render() {
     return (
       <div className="LS0401__screen">
-        <SubMenu title="Title" desc="Desc" detailDesc="DetailDesc Text" />
+        {menuList.map((menu) => {
+          return (
+            <SubMenu
+              title={menu.title}
+              desc={menu.desc}
+              detailDesc={menu.detailDesc}
+            />
+          );
+        })}
       </div>
     );
   }
