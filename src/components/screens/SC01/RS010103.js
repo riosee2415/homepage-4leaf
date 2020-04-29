906addabf1859de2a2d5915e5d561055b21f69d9
4leaf.ysh <4leaf.ysh@gmail.com>
2020-04-29 10:30:41 +0900

    RS01 SUPPORT

--- src/components/screens/SC01/RS010103.js
+++ src/components/screens/SC01/RS010103.js
@@ -1,12 +1,33 @@
 import React from "react";
 
 class RS0101 extends React.Component {
+  state = {
+    imgURL: "",
+    isLoading: false,
+  };
+
+  componentDidMount = async () => {
+    await this.setState({
+      imgURL: "../../assets/ddafsd/asdfasdf",
+      isLoading: true,
+    });
+  };
+
   render() {
+    const { isLoading, imgURL } = this.state;
+
     return (
       <div className="RS010103__screen">
         <div className="RS010103__content">
           <div className="RS010103__mainTitle">#조직도</div>
-          <div className="organization__image"></div>
+          {isLoading ? (
+            <>
+              <div className="organization__image"></div>
+              <img src={imgURL} alt="조직도" />
+            </>
+          ) : (
+            <div>이미지 로딩 중...</div>
+          )}
         </div>
       </div>
     );
