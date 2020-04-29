07a91c33f52fc35c9ecdbebefb1dd322e19a8ed9
song <songsm5504@gmail.com>
2020-04-29 18:21:03 +0900

    0101

--- src/components/screens/SC01/RS010104.js
+++ src/components/screens/SC01/RS010104.js
@@ -6,18 +6,25 @@ class RS010104 extends React.Component {
     super(props);
   }
 
-  map;
-  markers = [];
-  infowindows = [];
-
   componentDidMount() {
     var container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
     var options = {
       //지도를 생성할 때 필요한 기본 옵션
       center: new kakao.maps.LatLng(36.351046, 127.371009), //지도의 중심좌표.
-      level: 4, //지도의 레벨(확대, 축소 정도)
+      level: 3, //지도의 레벨(확대, 축소 정도)
     };
-    this.map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+    // 마커가 표시될 위치입니다
+    var markerPosition = new kakao.maps.LatLng(36.351046, 127.371009);
+
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+      position: markerPosition,
+    });
+
+    // 마커가 지도 위에 표시되도록 설정합니다
+    marker.setMap(map);
   }
 
   render() {
@@ -26,7 +33,7 @@ class RS010104 extends React.Component {
         <div className="RS010104__content">
           <div className="RS010104__mainTitle">#오시는 길</div>
           <div id="map" className="businessAddress"></div>
-          호출해야하는 것을 fetch로 받아서 불러오기
+          <div>🚩대전광역시 서구 계룡로394번길 14-14</div>
         </div>
       </div>
     );
