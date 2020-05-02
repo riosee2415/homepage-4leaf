6f0a6ff8a0eab7c747de10977d0ecd0e5f550730
song <songsm5504@gmail.com>
2020-05-02 22:03:46 +0900

    0101

--- src/components/screens/SC01/RS010102.js
+++ src/components/screens/SC01/RS010102.js
@@ -8,10 +8,12 @@ class RS010102 extends React.Component {
           <div className="RS010102__mainTitle">#연혁</div>
           <div>
             <div className="bar"></div>
-            <ul className="RS010102__des">
-              <li className="RS010102__year">🍀2020</li>
-              <li>4LEAF SOFTWARE 창립</li>
-            </ul>
+            <div>
+              <ul className="RS010102__des">
+                <li className="RS010102__year">🍀2020</li>
+                <li>4LEAF SOFTWARE 창립</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
