87206f831465520cb47c93e11e1b2887510cd0fb
song <songsm5504@gmail.com>
2020-05-06 18:03:49 +0900

    0101

--- src/components/screens/SC01/RS010103.js
+++ src/components/screens/SC01/RS010103.js
@@ -7,7 +7,10 @@ class RS010103 extends React.Component {
         <div className="RS010103__content">
           <div className="RS010103__mainTitle">#조직도</div>
           <div className="RS010103__imgWrap">
-            <div className="RS010103__des">체계적인 조직 구조로...</div>
+            <div>
+              체계적인 조직 구조로 고객님의 니즈를 반영한 맞춤형 결과물을
+              보여드리겠습니다.
+            </div>
             <div className="organization__image"></div>
           </div>
         </div>
